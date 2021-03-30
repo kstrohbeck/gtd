d2fb4f88a8d984f6421bc94247fe7a45169191cd
Kyle Strohbeck <kstrohbeck@gmail.com>
2021-03-30 13:13:02 -0500

    Use project status instead of project list.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use self::action_list::ActionList;
 use self::project::{Project, Status as ProjectStatus};
-use self::project_list::ProjectList;
 use self::someday_list::SomedayList;
 use argh::FromArgs;
 use std::{collections::HashSet, convert::AsRef, env, fs, path::Path};
@@ -9,7 +8,6 @@ mod action_list;
 mod markdown;
 mod parser;
 mod project;
-mod project_list;
 mod someday_list;
 
 /// Task management application.
@@ -73,43 +71,6 @@ fn main() {
                     Ok(())
                 }
 
-                fn verify_project_is_in_exactly_one_state(
-                    project: &Project,
-                    project_list: &ProjectList,
-                    someday_list: &SomedayList,
-                ) -> Result<(), String> {
-                    let is_in_project_list = project_list.contains(&project.filename);
-                    let is_in_someday_list = someday_list.contains(&project.filename);
-
-                    match (
-                        is_in_project_list,
-                        is_in_someday_list,
-                        project.status == ProjectStatus::Complete,
-                    ) {
-                        (false, false, false) => Err(format!(
-                            "{} is not marked complete and is not in the project or someday lists",
-                            project
-                        )),
-                        (false, true, true) => Err(format!(
-                            "{} is marked complete but is in the someday list",
-                            project
-                        )),
-                        (true, false, true) => Err(format!(
-                            "{} is marked complete but is in the project list",
-                            project
-                        )),
-                        (true, true, false) => Err(format!(
-                            "{} is in both the project and someday lists",
-                            project
-                        )),
-                        (true, true, true) => Err(format!(
-                            "{} is marked complete but is in both the project and someday lists",
-                            project
-                        )),
-                        _ => Ok(()),
-                    }
-                }
-
                 fn verify_all_actions_complete(project: &Project) -> Result<(), String> {
                     let are_all_actions_complete = project.actions.iter().all(|(x, _)| *x);
                     if project.status == ProjectStatus::Complete && !are_all_actions_complete {
@@ -125,11 +86,6 @@ fn main() {
                 let validations = vec![
                     validate_id(project, ids),
                     validate_title(project),
-                    verify_project_is_in_exactly_one_state(
-                        project,
-                        &docs.project_list,
-                        &docs.someday_list,
-                    ),
                     verify_all_actions_complete(project),
                 ];
 
@@ -147,35 +103,27 @@ fn main() {
                 validate_project(project, &docs, &mut ids);
             }
 
-            let mut project_list_links = HashSet::new();
-
-            for link in &docs.project_list.items {
-                let link = link as &str;
-
-                if let Some(project) = docs.projects.iter().find(|p| p.filename == link) {
-                    let mut has_active_action = false;
-                    for action in project.actions.iter().map(|(_, f)| f) {
-                        for ctx in &docs.action_list.contexts {
-                            for act in &ctx.actions {
-                                if action == &act.text {
-                                    has_active_action = true;
-                                }
+            for project in docs
+                .projects
+                .iter()
+                .filter(|p| p.status == ProjectStatus::InProgress)
+            {
+                let mut has_active_action = false;
+                for action in project.actions.iter().map(|(_, f)| f) {
+                    for ctx in &docs.action_list.contexts {
+                        for act in &ctx.actions {
+                            if action == &act.text {
+                                has_active_action = true;
                             }
                         }
                     }
-
-                    if !has_active_action {
-                        println!(
-                            "{} is in project list but has no actions in action list",
-                            project
-                        );
-                    }
-                } else {
-                    println!("{} is an invalid link in the project list", link);
                 }
 
-                if !project_list_links.insert(link) {
-                    println!("{} is duplicated in the project list", link);
+                if !has_active_action {
+                    println!(
+                        "{} is in project list but has no actions in action list",
+                        project
+                    );
                 }
             }
 
@@ -196,7 +144,7 @@ fn main() {
                     if let Some(link) = &action.project {
                         let link = link as &str;
                         if let Some(project) = docs.projects.iter().find(|p| p.filename == link) {
-                            if !docs.project_list.contains(link) {
+                            if project.status != ProjectStatus::InProgress {
                                 println!("{} is referenced in the action list but is not in the project list", link);
                             }
 
@@ -226,7 +174,7 @@ fn main() {
 #[derive(Debug)]
 struct Documents {
     action_list: ActionList,
-    project_list: ProjectList,
+    //project_list: ProjectList,
     someday_list: SomedayList,
     projects: Vec<Project>,
 }
@@ -239,11 +187,13 @@ impl Documents {
             ActionList::parse(&text).unwrap()
         }
 
+        /*
         fn load_project_list<P: AsRef<Path>>(cur_dir: P) -> ProjectList {
             let path = cur_dir.as_ref().join("Projects.md");
             let text = fs::read_to_string(&path).unwrap();
             ProjectList::parse(&text).unwrap()
         }
+        */
 
         fn load_someday_list<P: AsRef<Path>>(cur_dir: P) -> SomedayList {
             let path = cur_dir.as_ref().join("Someday.md");
@@ -268,7 +218,7 @@ impl Documents {
         let cur_dir = cur_dir.as_ref();
         Self {
             action_list: load_action_list(cur_dir),
-            project_list: load_project_list(cur_dir),
+            //project_list: load_project_list(cur_dir),
             someday_list: load_someday_list(cur_dir),
             projects: load_projects(cur_dir).collect(),
         }
