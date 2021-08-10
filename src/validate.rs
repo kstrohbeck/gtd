f3e724ccd849a8f8477c11fb1f56add2215cfcf6
Kyle Strohbeck <kstrohbeck@gmail.com>
2021-08-10 11:56:31 -0500

    Use Cow instead of static strings.

--- src/validate.rs
+++ src/validate.rs
@@ -3,7 +3,7 @@ use crate::{
     gtd::Documents,
     project::{ActionStatus, Project, Status as ProjectStatus},
 };
-use std::collections::HashSet;
+use std::{borrow::Cow, collections::HashSet};
 
 pub fn validate(docs: Documents) {
     ValidatorRunner::new()
@@ -20,19 +20,19 @@ pub fn validate(docs: Documents) {
         .run(&docs);
 }
 
-fn project_id_is_unique() -> impl FnMut(&Project) -> Result<(), &'static str> {
+fn project_id_is_unique() -> impl FnMut(&Project) -> Result<(), Cow<'static, str>> {
     let mut project_ids = HashSet::new();
 
     move |project| {
         if !project_ids.insert(project.id().to_string()) {
-            return Err("has a duplicate ID");
+            return Err("has a duplicate ID".into());
         }
 
         Ok(())
     }
 }
 
-fn project_title_matches_name(project: &Project) -> Result<(), &'static str> {
+fn project_title_matches_name(project: &Project) -> Result<(), Cow<'static, str>> {
     let name_title = project.title();
 
     let body_title = project
@@ -41,13 +41,13 @@ fn project_title_matches_name(project: &Project) -> Result<(), &'static str> {
         .ok_or("has an invalid title in its body")?;
 
     if name_title != body_title {
-        return Err("name and body title don't match");
+        return Err("name and body title don't match".into());
     }
 
     Ok(())
 }
 
-fn complete_project_has_only_complete_actions(project: &Project) -> Result<(), &'static str> {
+fn complete_project_has_only_complete_actions(project: &Project) -> Result<(), Cow<'static, str>> {
     if project.status != ProjectStatus::Complete {
         return Ok(());
     }
@@ -58,13 +58,13 @@ fn complete_project_has_only_complete_actions(project: &Project) -> Result<(), &
         .all(|(_, status)| status == ActionStatus::Complete);
 
     if !are_all_actions_complete {
-        return Err("is complete but has at least one uncomplete action");
+        return Err("is complete but has at least one uncomplete action".into());
     }
 
     Ok(())
 }
 
-fn in_progress_project_has_active_actions(project: &Project) -> Result<(), &'static str> {
+fn in_progress_project_has_active_actions(project: &Project) -> Result<(), Cow<'static, str>> {
     if project.status != ProjectStatus::InProgress {
         return Ok(());
     }
@@ -77,7 +77,7 @@ fn in_progress_project_has_active_actions(project: &Project) -> Result<(), &'sta
         >= 1;
 
     if !has_active_action {
-        return Err("is in progress but has no active actions");
+        return Err("is in progress but has no active actions".into());
     }
 
     Ok(())
@@ -95,10 +95,10 @@ macro_rules! unwrap_or_ok {
 fn action_link_is_valid(
     action: &ContextAction,
     project: Option<&Project>,
-) -> Result<(), &'static str> {
+) -> Result<(), Cow<'static, str>> {
     let _action_ref = unwrap_or_ok!(action.to_action_ref());
     if project.is_none() {
-        return Err("not a valid link to project");
+        return Err("not a valid link to project".into());
     }
 
     Ok(())
@@ -107,12 +107,12 @@ fn action_link_is_valid(
 fn linked_project_is_in_progress(
     action: &ContextAction,
     project: Option<&Project>,
-) -> Result<(), &'static str> {
+) -> Result<(), Cow<'static, str>> {
     let _action_ref = unwrap_or_ok!(action.to_action_ref());
     let project = unwrap_or_ok!(project);
 
     if project.status != ProjectStatus::InProgress {
-        return Err("linked project is not in progress");
+        return Err("linked project is not in progress".into());
     }
 
     Ok(())
@@ -121,12 +121,12 @@ fn linked_project_is_in_progress(
 fn linked_project_contains_action(
     action: &ContextAction,
     project: Option<&Project>,
-) -> Result<(), &'static str> {
+) -> Result<(), Cow<'static, str>> {
     let action_ref = unwrap_or_ok!(action.to_action_ref());
     let project = unwrap_or_ok!(project);
 
     if project.actions.get_action(&action_ref.action_id).is_none() {
-        return Err("linked project doesn't have the action");
+        return Err("linked project doesn't have the action".into());
     }
 
     Ok(())
@@ -135,25 +135,25 @@ fn linked_project_contains_action(
 fn action_in_project_is_active(
     action: &ContextAction,
     project: Option<&Project>,
-) -> Result<(), &'static str> {
+) -> Result<(), Cow<'static, str>> {
     let action_ref = unwrap_or_ok!(action.to_action_ref());
     let project = unwrap_or_ok!(project);
     let (_, status) = unwrap_or_ok!(project.actions.get_action(&action_ref.action_id));
 
     if status != ActionStatus::Active {
-        return Err("action is not active in linked project");
+        return Err("action is not active in linked project".into());
     }
 
     Ok(())
 }
 
 fn linked_action_is_unique(
-) -> impl FnMut(&ContextAction, Option<&Project>) -> Result<(), &'static str> {
+) -> impl FnMut(&ContextAction, Option<&Project>) -> Result<(), Cow<'static, str>> {
     let mut actions = HashSet::new();
     move |action, _project| {
         if let Some(action_ref) = action.to_action_ref() {
             if !actions.insert(action_ref.clone()) {
-                return Err("action is not unique in contexts");
+                return Err("action is not unique in contexts".into());
             }
         }
 
@@ -207,14 +207,14 @@ fn all_active_actions_are_in_a_context(docs: &Documents) {
 }
 
 trait ProjectValidator {
-    fn validate(&mut self, project: &Project) -> Result<(), &'static str>;
+    fn validate(&mut self, project: &Project) -> Result<(), Cow<'static, str>>;
 }
 
 impl<F> ProjectValidator for F
 where
-    F: FnMut(&Project) -> Result<(), &'static str>,
+    F: FnMut(&Project) -> Result<(), Cow<'static, str>>,
 {
-    fn validate(&mut self, project: &Project) -> Result<(), &'static str> {
+    fn validate(&mut self, project: &Project) -> Result<(), Cow<'static, str>> {
         self(project)
     }
 }
@@ -224,18 +224,18 @@ trait ContextActionValidator {
         &mut self,
         action: &ContextAction,
         project: Option<&Project>,
-    ) -> Result<(), &'static str>;
+    ) -> Result<(), Cow<'static, str>>;
 }
 
 impl<F> ContextActionValidator for F
 where
-    F: FnMut(&ContextAction, Option<&Project>) -> Result<(), &'static str>,
+    F: FnMut(&ContextAction, Option<&Project>) -> Result<(), Cow<'static, str>>,
 {
     fn validate(
         &mut self,
         action: &ContextAction,
         project: Option<&Project>,
-    ) -> Result<(), &'static str> {
+    ) -> Result<(), Cow<'static, str>> {
         self(action, project)
     }
 }
@@ -266,7 +266,7 @@ impl<'a> ValidatorRunner<'a> {
 
     pub fn for_all_projects<F>(mut self, validator: F) -> Self
     where
-        F: FnMut(&Project) -> Result<(), &'static str> + 'a,
+        F: FnMut(&Project) -> Result<(), Cow<'static, str>> + 'a,
     {
         self.project_validators.push(Box::new(validator));
         self
@@ -274,7 +274,7 @@ impl<'a> ValidatorRunner<'a> {
 
     pub fn for_all_context_actions<F>(mut self, validator: F) -> Self
     where
-        F: FnMut(&ContextAction, Option<&Project>) -> Result<(), &'static str> + 'a,
+        F: FnMut(&ContextAction, Option<&Project>) -> Result<(), Cow<'static, str>> + 'a,
     {
         self.context_action_validators.push(Box::new(validator));
         self
